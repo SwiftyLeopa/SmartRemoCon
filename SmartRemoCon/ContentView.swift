f467fce7af70eabfd2e8f0fec26bc7154992e069
leopa <leopa@leopa.com>
2020-07-27 19:49:13 +0900

    Initial Commit

---
+++ SmartRemoCon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartRemoCon
+//
+//  Created by 高木郁哉 on 2020/07/27.
+//  Copyright © 2020 swiftyLeopa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
